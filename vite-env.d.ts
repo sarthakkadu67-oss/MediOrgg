42337443408d4f46e58ed08102610f9bd652811b
Sarthak Kadu <sarthakkadu67@gmail.com>
2025-11-26 23:04:46 +0530

    Create vite-env.d.ts

---
+++ vite-env.d.ts
@@ -0,0 +1,48 @@
+// Manual type definitions for Vite client environment
+// Used when vite/client types are unavailable
+
+declare module '*.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module '*.svg' {
+  import * as React from 'react';
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement> & { title?: string }>;
+  const src: string;
+  export default src;
+}
+
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpeg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.gif' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.webp' {
+  const src: string;
+  export default src;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_API_KEY: string;
+  readonly [key: string]: string | boolean | undefined;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
